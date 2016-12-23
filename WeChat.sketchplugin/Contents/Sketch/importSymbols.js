e7ecb6fa4f84b9dd416df87175f5bbb3de695a88
bugluo <6185763@qq.com>
2016-12-23 12:23:32 +0800

    存一个中间版本。

--- WeChat.sketchplugin/Contents/Sketch/importSymbols.js
+++ WeChat.sketchplugin/Contents/Sketch/importSymbols.js
@@ -1,8 +1,9 @@
 @import "config.js"
 @import "common.js"
+@import "organizer.js"
 
 var onRun = function (context) {
-	context.document.showMessage("检查更新中...");
+	context.document.showMessage("下载更新中...");
 	var theResponseData = request(UIKIT);
 	var data = [[NSData alloc] initWithData:theResponseData];
 	var basepath = [[NSFileManager defaultManager] currentDirectoryPath];
@@ -21,12 +22,84 @@ var onRun = function (context) {
 		var CTCount = 0;
 		var addPageCount = 0;
 
+		//先处理symbol
+		(function(){
+			var sourceSymbol = sourceDoc.documentData().allSymbols();
+			var localSymobl = doc.documentData().allSymbols();
+			var pushAllArtboards = [];
+			var deleteObject = {};
+
+	        for(var f=0;f<sourceSymbol.count();f++){
+	        	var s = sourceSymbol[f];
+	        	var flagForNewSymbol = false;
+	        	for(var g=0;g<localSymobl.count();g++){
+	        		if(encodeURI(s.name()) == encodeURI(localSymobl[g].name())){
+	        			flagForNewSymbol = true;
+
+	        			// 找出相同名字的，内容相同不处理，内容不同，使用source的，并把local的放到save
+	        			if(!isSame(s,localSymobl[g])){
+	        				if(CTCount == 0){
+	        					savePage = doc.addBlankPage();
+								savePage.setName('更新冲突');
+	        				}
+	        				CTCount ++;
+	        				//添加原始的到冲突
+	        				var lolG = localSymobl[g];
+	        				savePage.addLayers([lolG]);
+	        				//删除原始的
+	        				localSymobl[g].removeFromParent();
+	        				//添加线上的到画布
+	        				var newSymbol = s;
+	        				var rect = s.rect();
+	        				newSymbol.rect = rect;
+	        				pushAllArtboards.push(newSymbol);
+	        			}
+	        			deleteObject[g] = true;
+	        			break;
+	        		}
+	        	}
+	        	if(!flagForNewSymbol){
+	        		//没找着，直接添加source的到现有的画布
+	        		var newSymbol = s;
+					var rect = s.rect();
+					newSymbol.rect = rect;
+					pushAllArtboards.push(newSymbol);
+	        		
+	        		addSymbolCount++;
+	        	}
+	        }
+	        //没找着，直接添加source的到冲突画布
+	        for(var g=0;g<localSymobl.count();g++){
+	        	if(!deleteObject[g]){
+	        		if(CTCount == 0){
+						savePage = doc.addBlankPage();
+						savePage.setName('更新冲突');
+					}
+	        		CTCount ++;
+	        		var saveSymbol = localSymobl[g];
+	        		savePage.addLayers([saveSymbol]);
+	        	}
+	        }
+	        doc.setCurrentPage(doc.documentData().symbolsPageOrCreateIfNecessary());
+			var currentPage = context.document.currentPage();
+			currentPage.addLayers(pushAllArtboards);
+		})()
+		
+
+
+		//再处理页面
 
 		for(var i=0;i<sourcePages.count();i++){
 			var sourcePageName = sourcePages[i].name();
+			if(sourcePageName == 'Symbols'){
+				continue;
+			}
 			var flagForOldPage = false;
 			for(var k=0;k<pages.count();k++){
 				//如果有同一个page名
+				if(pages[k].name() == 'Symbols'){
+					continue;
+				}
 				if(encodeURI(pages[k].name()) == encodeURI(sourcePageName)){
 					flagForOldPage = true;
 
@@ -34,6 +107,7 @@ var onRun = function (context) {
 					var sourceSymbol = sourcePages[i].artboards();
 		   			var localSymobl = pages[k].artboards();
 		   			var deleteObject = {};
+		   			var pushAllArtboards = [];
 
 		            for(var f=0;f<sourceSymbol.count();f++){
 		            	var s = sourceSymbol[f];
@@ -50,12 +124,15 @@ var onRun = function (context) {
 		            				}
 		            				CTCount ++;
 		            				//添加原始的到冲突
-		            				savePage.addLayers([localSymobl[g].copy()]);
+		            				var lolG = localSymobl[g];
+		            				savePage.addLayers([lolG]);
 		            				//删除原始的
 		            				localSymobl[g].removeFromParent();
-		            				localSymobl[g].adjustFrameToFit();
 		            				//添加线上的到画布
-		            				pages[k].addLayers([s.copy()]);
+		            				var newSymbol = s;
+		            				var rect = s.rect();
+		            				newSymbol.rect = rect;
+		            				pushAllArtboards.push(newSymbol);
 		            			}
 		            			deleteObject[g] = true;
 		            			break;
@@ -63,7 +140,11 @@ var onRun = function (context) {
 		            	}
 		            	if(!flagForNewSymbol){
 		            		//没找着，直接添加source的到现有的画布
-		            		pages[k].addLayers([s.copy()]);
+		            		var newSymbol = s;
+            				var rect = s.rect();
+            				newSymbol.rect = rect;
+            				pushAllArtboards.push(newSymbol);
+		            		
 		            		addSymbolCount++;
 		            	}
 		            }
@@ -79,23 +160,38 @@ var onRun = function (context) {
 		            		savePage.addLayers([saveSymbol]);
 		            	}
 		            }
+		            pages[k].addLayers(pushAllArtboards);
 					break;
 				}
 			}
-			//如果没有
+			//如果没有直接添加一个新的page
+			// 不行，直接新增page有bug，没bug，要先添加symbol
 
 			if(!flagForOldPage){
-				addPageCount++;
+				addPageCount++;	
+				// var newPage = doc.addBlankPage();
+				// var sourceSymbol = sourcePages[i].artboards();
+				// var newArtBoards = [];
+				// for(var i = 0;i<sourceSymbol.count();i++){
+				// 	newArtBoards.push(sourceSymbol[i].copy());
+				// }
+				// newPage.setName(sourcePageName);
+				// newPage.addLayers(newArtBoards);
 				doc.documentData().addPage(sourcePages[i]);
 			}
-
+			doc.setCurrentPage(doc.documentData().symbolsPageOrCreateIfNecessary());
 		}
-		doc.setCurrentPage(doc.documentData().symbolsPageOrCreateIfNecessary());
-		NSApp.displayDialog('新增'+ addPageCount + '个页面，' + addSymbolCount + '个单元，有'+ CTCount + '个冲突，' +" UIKIT已经导入成功！");
+		
 	}
 	sourceDoc.close();
 	sourceDoc = nil;
-
+	var alertData = '新增'+ addPageCount + '个页面，' + addSymbolCount + '个组，有'+ CTCount + '个冲突，';
+	alertData += 'UIKIT已经导入成功！';
+	if(CTCount>0){
+		doc.setCurrentPage(savePage);
+		Organizer(context);
+	}
+	NSApp.displayDialog(alertData);
 }
 
 function isSame(a,b){
@@ -103,9 +199,6 @@ function isSame(a,b){
 
 	//视图不一样大
 	if(encodeURI(a.rect().toString()) != encodeURI(b.rect().toString())){
-		return false;
-	}
-	if(encodeURI(a.name()) != encodeURI(b.name())){
 		return false;
 	}
 
---
+++ WeChat.sketchplugin/Contents/Sketch/organizer.js
@@ -0,0 +1,257 @@
+//by Symobl Organizer
+
+
+var Organizer = function(context) {
+	// Document variables
+	var doc = context.document;
+	var command = context.command;
+	var page = doc.currentPage();
+	var symbols = page.artboards();
+	var symbolCount = symbols.count();
+
+
+	// If there are no symbols...
+	if (!symbolCount) {
+		// Feedback to user
+		displayDialog("没有需要整理的Layers");
+	}
+	// If there are symbols...
+	else {
+		// Reset page origin
+		var pageOrigin = CGPointMake(0,0);
+		page.setRulerBase(pageOrigin);
+
+		// Get layout settings
+		var layoutSettings = getLayoutSettings();
+
+		// If layout settings were provided...
+		if (layoutSettings) {
+			// Remove unused symbols
+			if (layoutSettings.removeSymbols == 1) {
+				var removedCount = 0;
+
+				for (var i=0; i < symbols.count(); i++) {
+					var symbol = symbols.objectAtIndex(i);
+
+					if (!symbol.hasInstances()) {
+						symbol.removeFromParent();
+						removedCount++;
+					}
+				}
+
+				symbols = page.artboards();
+				symbolCount = symbols.count();
+			}
+
+			// Duplicate symbols object
+			var layoutSymbols = page.artboards();
+
+			// Sort new symbols object by name
+			var sortByName = [NSSortDescriptor sortDescriptorWithKey:"name" ascending:1];
+			layoutSymbols = [layoutSymbols sortedArrayUsingDescriptors:[sortByName]];
+
+			// Duplicate the sorted symbol object, reverse the order if the user has selected to do so
+			var layoutLayers = (layoutSettings.sortOrder == 0) ? [[layoutSymbols reverseObjectEnumerator] allObjects] : layoutSymbols;
+
+			// Sort the layer list
+			sortLayerList(layoutLayers);
+
+			// Grouping variables
+			var groupCount = 0;
+			var groupLayout = [];
+			var lastGroupPrefix;
+
+			// Iterate through the symbols
+			for (var i = 0; i < symbolCount; i++) {
+				// Symbol variables
+				var symbol = layoutSymbols.objectAtIndex(i);
+				var symbolName = symbol.name();
+
+				// Determine a break point in the symbol name
+				var breakPoint = getCharPosition(symbolName,"/",layoutSettings.groupDepth+1);
+
+				// Set a prefix for current group
+				var thisGroupPrefix = (breakPoint > 0) ? symbolName.slice(0,breakPoint) : symbolName.slice(0,symbolName.indexOf("/"));
+
+				// If this group prefix is not the same as last group
+				if (lastGroupPrefix != thisGroupPrefix) {
+					// Increment the group counter
+					groupCount++;
+				}
+
+				// Add an entry to the group object
+				groupLayout.push({
+					prefix: thisGroupPrefix,
+					group: groupCount
+				});
+
+				// Set the last group prefix to current prefix
+				lastGroupPrefix = thisGroupPrefix;
+			}
+
+			// Layout variables
+			var x = 0;
+			var y = 0;
+			var xPad = parseInt(layoutSettings.xPad);
+			var yPad = parseInt(layoutSettings.yPad);
+			var groupSpace = 0;
+			var maxPer = (layoutSettings.maxPer > 0) ? layoutSettings.maxPer : 0;
+
+			// Reset the group counter
+			var groupCount = 1;
+			var objectCount = 1;
+
+			// Iterate through the group object
+			for (var i = 0; i < groupLayout.length; i++) {
+				// Symbol variables
+				var symbol = layoutSymbols.objectAtIndex(i);
+				var symbolFrame = symbol.frame();
+
+				// Layout symbols horizontally or vertically
+				if (layoutSettings.sortDirection == 0) {
+					// If the current group number doesn't match the group counter
+					if (groupLayout[i]['group'] != groupCount) {
+						// Reset x position, set the y position of the next row, reset the row height
+						y = 0;
+						x += groupSpace + xPad;
+						groupSpace = 0;
+
+						// Increment the group counter
+						groupCount++;
+
+						// Reset the object counter
+						objectCount = 1;
+					}
+
+					// If the max per row is greater than 0, and object count is greater than max per row
+					if (maxPer > 0 && objectCount > maxPer) {
+						// Reset x position, set the y position of the next row, reset the row height
+						y = 0;
+						x += groupSpace + xPad;
+						groupSpace = 0;
+
+						// Reset the object counter
+						objectCount = 1;
+					}
+
+					// Position the symbol
+					symbolFrame.x = x;
+					symbolFrame.y = y;
+
+					// If this symbol is taller than previous symbols in row
+					if (symbolFrame.width() > groupSpace) {
+						// Increase the height of the row
+						groupSpace = symbolFrame.width();
+					}
+
+					// Set the y position for the next symbol
+					y += symbolFrame.height() + yPad;
+				} else {
+					// If the current group number doesn't match the group counter
+					if (groupLayout[i]['group'] != groupCount) {
+						// Reset x position, set the y position of the next row, reset the row height
+						x = 0;
+						y += groupSpace + yPad;
+						groupSpace = 0;
+
+						// Increment the group counter
+						groupCount++;
+
+						// Reset the object counter
+						objectCount = 1;
+					}
+
+					// If the max per row is greater than 0, and object count is greater than max per row
+					if (maxPer > 0 && objectCount > maxPer) {
+						// Reset x position, set the y position of the next row, reset the row height
+						x = 0;
+						y += groupSpace + yPad;
+						groupSpace = 0;
+
+						// Reset the object counter
+						objectCount = 1;
+					}
+
+					// Position the symbol
+					symbolFrame.x = x;
+					symbolFrame.y = y;
+
+					// If this symbol is taller than previous symbols in row
+					if (symbolFrame.height() > groupSpace) {
+						// Increase the height of the row
+						groupSpace = symbolFrame.height();
+					}
+
+					// Set the x position for the next symbol
+					x += symbolFrame.width() + xPad;
+				}
+
+				// Increment the object counter
+				objectCount++;
+			}
+		}
+	}
+
+
+	function getLayoutSettings() {
+		// Setting variables
+		var groupOptions = ['Label/','Label/Label/','Label/Label/Label/','Label/Label/Label/Label/'];
+		var groupDepth = 1;
+		var sortOrderOptions = ['A-Z','Z-A'];
+		var sortOrder = 1;
+		var sortDirectionOptions = ['Horizontal','Vertical'];
+		var sortDirection = 0;
+		var xPad = '300';
+		var yPad = '300';
+		var removeSymbols = 0;
+		var maxPer = '';
+
+
+		return {
+			groupDepth : groupDepth,
+			sortOrder : sortOrder,
+			sortDirection : sortDirection,
+			maxPer : maxPer,
+			xPad : xPad,
+			yPad : yPad,
+			removeSymbols : removeSymbols
+		}
+	}
+	function displayDialog(title,body) {
+		var app = NSApplication.sharedApplication();
+		app.displayDialog_withTitle(body,title);
+	}
+
+	function getCharPosition(string,match,count) {
+		var position = string.split(match,count).join(match).length;
+
+		if (string.length() != position) {
+			return position;
+		} else return -1;
+	}
+
+	function sortLayerList(layoutSymbols) {
+		var parent = page;
+		var indices = [];
+		var loop = [symbols objectEnumerator], symbol;
+
+		while (symbol = [loop nextObject]) {
+			indices.push(parent.indexOfLayer(symbol));
+		}
+
+		var removeLoop = [symbols objectEnumerator], symbolToRemove;
+
+		while (symbolToRemove = [removeLoop nextObject]) {
+			[symbolToRemove removeFromParent];
+		}
+
+		for (var i = 0; i < indices.length; i++) {
+			var index = indices[i];
+			var sortedSymbol = layoutSymbols[i];
+			var layerArray = [NSArray arrayWithObject:sortedSymbol];
+			[parent insertLayers:layerArray atIndex:index];
+		}
+
+		symbols = page.artboards();
+	}
+};
