64e57ff26f405ea467fc52b03afe3c340b0e797b
bugluo <6185763@qq.com>
2017-01-09 00:24:17 +0800

    增加一个新功能，全文文字替换 。

--- WeChat nb.sketchplugin/Contents/Sketch/importSymbols.js
+++ WeChat nb.sketchplugin/Contents/Sketch/importSymbols.js
@@ -7,10 +7,8 @@ function chooseKit(context){
 	settingsWindow.addButtonWithTitle("取消");
 
 	settingsWindow.setMessageText("请选择您需要同步的UIkit");
-	settingsWindow.setInformativeText("");
+	settingsWindow.setInformativeText("不能在同一个项目中同步不同的UIkit，否则会产生错误");
     
-    settingsWindow.addTextLabelWithValue("选择");
-
 	var List = getConfig('config',context).UIKIT;
 	var ButtonList = [];
 
