a7debc08de0c1727740818e70b398ffe84e0b8ae
bugluo <6185763@qq.com>
2017-05-15 10:41:20 +0800

    setting

--- WeSketch.sketchplugin/Contents/Sketch/keySetting.js
+++ WeSketch.sketchplugin/Contents/Sketch/keySetting.js
@@ -3,13 +3,25 @@
 
 var onRun = function(context){
 
-    var pluginSketch = context.plugin.url().URLByAppendingPathComponent("Contents").URLByAppendingPathComponent("Sketch").URLByAppendingPathComponent("library").path();
+    var manifestPath = context.plugin.url().URLByAppendingPathComponent("Contents").URLByAppendingPathComponent("Sketch").URLByAppendingPathComponent("manifest.json").path(),
+        manifest = NSJSONSerialization.JSONObjectWithData_options_error(NSData.dataWithContentsOfFile(manifestPath), NSJSONReadingMutableContainers, nil),
+        commands = manifest.commands;
+       NSApp.displayDialog(JSON.stringity(commands));
 
+    var pluginSketch = context.plugin.url().URLByAppendingPathComponent("Contents").URLByAppendingPathComponent("Sketch").URLByAppendingPathComponent("library").path();
+    var obj = [];
+    for(var i = 0;i < commands.length -1;i++){
+        obj.push({name:encodeURIComponent(commands[i].name),shortcut:encodeURIComponent(commands[i].shortcut)})
+    }
+       NSApp.displayDialog(JSON.stringity(obj));
 
 	SMPanel({
-        url: pluginSketch + "/panel/setting.html",
+        url: pluginSketch + "/panel/keySetting.html",
         width: 362,
         height: 548,
+        data:{
+            commands:obj
+        },
         hiddenClose: false,
         floatWindow: true,
         identifier: "keySetting",
