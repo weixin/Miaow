8259f784cdb2c83211a8a7e2684ca020d8b9d163
bugluo <6185763@qq.com>
2017-05-16 22:48:02 +0800

    解决读取svg的问题。

--- WeSketch.sketchplugin/Contents/Sketch/uploadIcon.js
+++ WeSketch.sketchplugin/Contents/Sketch/uploadIcon.js
@@ -1,23 +1,31 @@
 @import "common.js";
 
-function exportSVG(layer,doc,file){
+function choiceSVG(layer,doc){
     var slice = MSExportRequest.exportRequestsFromExportableLayer(layer).firstObject();
     slice.scale = '1';
     slice.format = 'svg';
-    // var savePath = file + '/svg/' + layer.name() + '.svg';
+    var save = NSSavePanel.savePanel();
+    var savePath = save.URL().path() + '.svg';
     doc.saveArtboardOrSlice_toFile(slice, savePath);
-    var content = networkRequest([savePath]);
-    log(content);
+    var content = NSData.dataWithContentsOfURL(NSURL.URLWithString('file://'+savePath));
+    var string = [[NSString alloc] initWithData:content encoding:NSUTF8StringEncoding];
+    return string;
 }
 
 var onRun = function(context){
 
+    var selection = context.selection;
+    if(selection.length == 1){
+        selection = selection[0];
+    }
+    var svg = encodeURIComponent(choiceSVG(selection,context.document));
     var pluginSketch = context.plugin.url().URLByAppendingPathComponent("Contents").URLByAppendingPathComponent("Sketch").URLByAppendingPathComponent("library").path();
 
 	SMPanel({
         url: pluginSketch + "/panel/uploadIcon.html",
         width: 362,
         height: 548,
+        data:{svg:svg}
         hiddenClose: false,
         floatWindow: true,
         identifier: "uploadIcon",
