12dca14b97f678f2294ac9ae3c0a70c727cc4856
bugluo <6185763@qq.com>
2017-06-28 16:04:31 +0800

    实现多语言的切换。

--- WeSketch.sketchplugin/Contents/Sketch/setting.js
+++ WeSketch.sketchplugin/Contents/Sketch/setting.js
@@ -3,8 +3,21 @@
 var onRun = function(context){
     var i18 = _(context).setting;
 
+    var i18nKey = "com.sketchplugins.wechat.i18n";
     var toolbarAutoShow = "com.sketchplugins.wechat.toolbarautoshow";
     var updateAutoShow = "com.sketchplugins.wechat.updateAutoShow";
+
+    var lang = NSUserDefaults.standardUserDefaults().objectForKey(i18nKey);
+    if(lang == undefined){
+        var macOSVersion = NSDictionary.dictionaryWithContentsOfFile("/System/Library/CoreServices/SystemVersion.plist").objectForKey("ProductVersion") + "";
+        lang = NSUserDefaults.standardUserDefaults().objectForKey("AppleLanguages").objectAtIndex(0);
+        lang = (macOSVersion >= "10.12")? lang.split("-").slice(0, -1).join("-"): lang;
+        if(lang.indexOf('zh') > -1){
+            lang = 'zh';
+        }else{
+            lang = 'en';
+        }
+    }
 
     var manifestPath = context.plugin.url().URLByAppendingPathComponent("Contents").URLByAppendingPathComponent("Sketch").URLByAppendingPathComponent("manifest.json").path(),
         manifest = NSJSONSerialization.JSONObjectWithData_options_error(NSData.dataWithContentsOfFile(manifestPath), NSJSONReadingMutableContainers, nil),
@@ -14,13 +27,12 @@ var onRun = function(context){
     var obj = [];
     for(var i = 0;i < commands.length;i++){
         if(!commands[i].noshortcut){
-            obj.push({name:encodeURIComponent(commands[i].name),shortcut:encodeURIComponent(commands[i].shortcut),istool:encodeURIComponent(commands[i].istool)})
+            obj.push({identifier:encodeURIComponent(commands[i].identifier),name:encodeURIComponent(commands[i].name),shortcut:encodeURIComponent(commands[i].shortcut),istool:encodeURIComponent(commands[i].istool)})
         }
     }
 
     var toolbarAuto = NSUserDefaults.standardUserDefaults().objectForKey(toolbarAutoShow) || '';
     var updateAuto = NSUserDefaults.standardUserDefaults().objectForKey(updateAutoShow) || '';
-
 
 	SMPanel({
         url: pluginSketch + "/panel/setting.html",
@@ -30,27 +42,32 @@ var onRun = function(context){
             commands:obj,
             toolbarAuto:encodeURIComponent(toolbarAuto),
             updateAuto:encodeURIComponent(updateAuto),
-            i18:i18
+            i18:i18,
+            language:encodeURIComponent(lang)
         },
         hiddenClose: false,
         floatWindow: true,
         identifier: "setting",
         callback: function( data ){
+            var manifestPathLang = context.plugin.url().URLByAppendingPathComponent("Contents").URLByAppendingPathComponent("Sketch").URLByAppendingPathComponent("i18n").URLByAppendingPathComponent("manifest-"+data.language+".json").path(),
+            manifestLang = NSJSONSerialization.JSONObjectWithData_options_error(NSData.dataWithContentsOfFile(manifestPathLang), NSJSONReadingMutableContainers, nil),
+            commandsLang = manifestLang.commands;
             var keydata = data.keydata;
-            for(var i = 0;i < commands.length;i++){
+            for(var i = 0;i < commandsLang.length;i++){
                 for(var k = 0; k < keydata.length;k++){
-                    if(decodeURIComponent(keydata[k].name) == (commands[i].name)){
-                        commands[i].shortcut = decodeURIComponent(keydata[k].shortcut);
-                        commands[i].istool = (keydata[k].istool);
+                    if(decodeURIComponent(keydata[k].identifier) == (commandsLang[i].identifier)){
+                        commandsLang[i].shortcut = decodeURIComponent(keydata[k].shortcut);
+                        commandsLang[i].istool = (keydata[k].istool);
                     }
                 }
             }
-            manifest.commands = commands;
+            manifest.commands = commandsLang;
             NSString.alloc().initWithData_encoding(NSJSONSerialization.dataWithJSONObject_options_error(manifest, NSJSONWritingPrettyPrinted, nil), NSUTF8StringEncoding).writeToFile_atomically_encoding_error(manifestPath, true, NSUTF8StringEncoding, nil);
             AppController.sharedInstance().pluginManager().reloadPlugins();
             
             NSUserDefaults.standardUserDefaults().setObject_forKey(data.updateAuto, updateAutoShow);
             NSUserDefaults.standardUserDefaults().setObject_forKey(data.toolbarAuto, toolbarAutoShow);
+            NSUserDefaults.standardUserDefaults().setObject_forKey(data.language, i18nKey);
 
             context.document.showMessage(i18.m1);
 
