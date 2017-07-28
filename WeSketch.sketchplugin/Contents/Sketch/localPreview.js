0fad904a5b7ee31067135f4607d43aad5a8ede98
bugluo <6185763@qq.com>
2017-07-28 17:51:43 +0800

    Revert "Merge branch 'master' into flag"
    This reverts commit 876cd65979a7679f886b3c75a3ecfb14c8087f3e, reversing
    changes made to 61ca3b9358192e14248c456da63841594370e4b0.

---
+++ WeSketch.sketchplugin/Contents/Sketch/localPreview.js
@@ -0,0 +1,26 @@
+@import "common.js"
+@import "link.js"
+@import "commonPreview.js"
+
+var localPreview =function(context){
+	function chooseFilePath(){
+		var save = NSSavePanel.savePanel();
+		save.setAllowsOtherFileTypes(true);
+		save.setExtensionHidden(false);
+		if(save.runModal()){
+			return save.URL().path();
+		}else{
+			return false;
+		}
+	}
+	var filePath = chooseFilePath();
+	if(!filePath){
+    	return;
+	}
+	commonCodeJson(context,filePath);
+    NSApp.displayDialog('导出成功');
+}
+
+var onRun = function(context){
+	localPreview(context);
+}
---
+++ WeSketch.sketchplugin/Contents/Sketch/webPreview.js
@@ -0,0 +1,49 @@
+@import "common.js"
+@import "link.js"
+@import "commonPreview.js"
+
+function webPreview(context){
+	function chooseFilePath(){
+		var save = NSSavePanel.savePanel();
+		save.setAllowsOtherFileTypes(true);
+		save.setExtensionHidden(false);
+		return save.URL().path()+'/'+(Math.random());
+	}
+	var filePath = chooseFilePath();
+	commonCodeJson(context,filePath);
+    zip(['-q','-r','-m','-o','-j',filePath+'.zip',filePath]);
+    var settingsWindow = COSAlertWindow.new();
+    settingsWindow.addButtonWithTitle('确定');
+    settingsWindow.addButtonWithTitle('取消');
+
+    settingsWindow.setMessageText('确定将页面传到服务器吗？');
+    settingsWindow.setInformativeText('警告，免费服务，作者不承担可能的泄漏风险，你也可以选择导出到本地自行预览');
+
+    if(settingsWindow.runModal() == "1000"){
+    	var returnData = networkRequest(['-F','image=@'+filePath+'.zip',iconQueryUrl+'/users/uploadHtml']);
+    	var jsonData = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    	jsonData = JSON.parse(jsonData);
+		var pluginSketch = context.plugin.url().URLByAppendingPathComponent("Contents").URLByAppendingPathComponent("Sketch").URLByAppendingPathComponent("library").path();
+		SMPanel({
+	        url: pluginSketch + "/panel/preview.html",
+	        width: 240,
+	        height: 280,
+	        data:{
+	        	link:iconQueryUrl.replace(':3000','')+'/'+jsonData.dir+'/index.html'
+	        },
+	        hiddenClose: false,
+	        floatWindow: true,
+	        identifier: "preview",
+	        callback: function( data ){
+                openUrlInBrowser(data.link);
+	        }
+	    });
+    }
+	var fm  =[NSFileManager defaultManager];
+    fm.removeItemAtPath_error(filePath,nil);
+    fm.removeItemAtPath_error(filePath+'.zip',nil);
+}
+
+var onRun = function(context){
+	webPreview(context);
+}
